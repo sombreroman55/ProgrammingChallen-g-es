308905297a7a448b26145c991bd82ae719fc5e47
Andrew Roberts <sombreroman55@gmail.com>
2018-10-02 15:36:24 -0600

    Small change to push

--- 010/list.h
+++ 010/list.h
@@ -16,16 +16,11 @@ namespace todo
       List(std::string _title);
       List(std::string _title, std::string _desc);
       ~List();
-      void addTask(Task t);
-      void removeTask();
+      void AddTask(Task t);
+      void RemoveTask();
+      void PrintTasks();
     private:
       std::vector<Task> tasks;  // The tasks associated with this list
-      std::string title;        // A short title describing the list
-      std::string description;  // An optional, detailed description of the list
-      auto creation_datetime = std::chrono::system_clock::now();
-      auto due_datetime;
-      bool completed = false;
-      Priority priority;
   };
 }
 
