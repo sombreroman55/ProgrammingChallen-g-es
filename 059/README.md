a60afa9162e9a768753454cd12b197032b705af7
Andrew Roberts <sombreroman55@gmail.com>
2021-11-22 13:13:42 -0700

    Finishing Caesar cipher

---
+++ 059/README.md
@@ -0,0 +1,43 @@
+# Challenge 059 - Caesar Cipher Cracker
+
+## Goals for This Challenge
+- A small scale program to
+
+## Summary
+This program is a small Caesar cipher decoder/cracker, but also an encoder.
+You can encode/decode with a known shift, and also dump all possible decodings.
+The implementation of the Caesar cipher is case insensitive, though it will
+maintain case during encoding/decoding.
+
+## Dependencies
+As this program is written in Nim, Nim needs to be installed on your machine.
+
+## Implementation
+The program is just a straightforward implementation of the [Caesar
+cipher](https://en.wikipedia.org/wiki/Caesar_cipher).
+No gory details necessary.
+
+## Installation
+No installation necessary. Compile the program normally and run it as detailed below.
+
+## Usage
+To encode a string:
+```bash
+./caesar encode <string to encode> <shift (letter or number)>
+```
+
+```bash
+./caesar decode <string to decode> <shift (letter or number)>
+```
+
+```bash
+./caesar dump-all <string to decode>
+```
+
+## Things That Can Be Improved
+I'm sure my Nim is very sloppy, but it was my first attempt. Not really sure how I feel
+about Nim based on first impressions, but hopefully my usage would improve with further
+practice.
+
+## License
+This code is licensed under the GNU GPL3 license.
--- 059/caesar.nim
+++ 059/caesar.nim
@@ -5,13 +5,91 @@ Implementation of a Caesar cipher encoder
 and decoder for Nim learning purposes and
 to do an easy /g/ challenge.
 ]#
+import os
+import std/strutils
+
+proc isAlpha(c: char): bool =
+  return c in Letters
+
 proc caesarEncode(s: string, shift: int): string =
   var
     output: string
+    c: char
+    upper: int
   for l in s:
-    var c = char(((int(l)-int('a') + shift) mod 26) + int('a'))
+    if 'a' <= l and l <= 'z':
+      upper = int('a')
+    elif 'A' <= l and l <= 'Z':
+      upper = int('A')
+    c = char(((int(l) - upper + (shift mod 26)) mod 26) + upper)
     output &= c
   return output
 
-var p = caesarEncode("abcdefg", 1)
-echo p
+proc caesarDecode(s: string, shift: int): string =
+  var
+    output: string
+    c: char
+    upper: int
+  for l in s:
+    if 'a' <= l and l <= 'z':
+      upper = int('a')
+    elif 'A' <= l and l <= 'Z':
+      upper = int('A')
+    c = char(((int(l) - upper + 26 - (shift mod 26)) mod 26) + upper)
+    output &= c
+  return output
+
+proc caesarDumpAllPossibleDecodings(s: string): array[26, string] =
+  var
+    allDecodings: array[26, string]
+  for i in 0 ..< 26:
+    allDecodings[i] = caesarDecode(s, i)
+  return allDecodings
+
+proc printUsage(): void =
+  echo "caesar usage:"
+  echo "  Encode a string: ./caesar encode <string to encode> <shift (letter or number)>"
+  echo "  Decode a string: ./caesar decode <string to decode> <shift (letter or number)>"
+  echo "  Dump all possible decodings: ./caesar dump-all <string to decode>"
+
+proc printError(error: string): void =
+  echo "Error: ", error
+  printUsage()
+
+if paramCount() == 2:
+  if paramStr(1) == "dump-all":
+    echo caesarDumpAllPossibleDecodings(paramStr(2))
+  else:
+    printUsage()
+
+elif paramCount() == 3:
+  var
+    shift: int = 0
+
+  if paramStr(1) == "encode":
+    try:
+      shift = parseInt(string(paramStr(3)))
+      echo caesarEncode(paramStr(2), shift)
+    except ValueError:
+      if isAlpha(paramStr(3)[0]):
+        shift = int(toLowerAscii(paramStr(3)[0])) - int('a')
+        echo caesarEncode(paramStr(2), shift)
+      else:
+        printError("Shift is not a letter or a number")
+
+  elif paramStr(1) == "decode":
+    try:
+      shift = parseInt(string(paramStr(3)))
+      echo caesarDecode(paramStr(2), shift)
+    except ValueError:
+      if isAlpha(paramStr(3)[0]):
+        shift = int(toLowerAscii(paramStr(3)[0])) - int('a')
+        echo caesarDecode(paramStr(2), shift)
+      else:
+        printError("Shift is not a letter or a number")
+
+  else:
+    printUsage()
+
+else:
+  printUsage()
